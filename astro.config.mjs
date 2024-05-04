d07836316db48fbce49025d037560a5605784c15
omarperez <omar.p@reflexmediagroup.com>
2024-05-04 23:09:19 +0200

    Github Actions modified deployment file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://omarjesusperezortiz.github.io/',
+    site: 'https://omarjesusperezortiz.github.io',
     base: 'resume',
 });
